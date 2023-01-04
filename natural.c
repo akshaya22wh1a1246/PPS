596c750b6eef82ec3e80566e1960fd832bf1dda6
akshaya22wh1a1246 <121919096+akshaya22wh1a1246@users.noreply.github.com>
2023-01-04 12:24:07 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,sum=0;
+	printf("\n Enter the value of n..");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+           printf("%d",i);
+	        sum=sum+i;
+    
+	}
+	printf("\n %d",sum);
+}
+	     	
